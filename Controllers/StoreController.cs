792ce002941bcd701919c91780c2aed2e06c8295
Khoi Nguyen Tan <khoi.nt.21@gmail.com>
2020-10-01 20:28:07 +0700

    FormData Post Action Stater

---
+++ Controllers/StoreController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace X.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoreController : ControllerBase
+    {
+        [HttpPost("a")]
+        public string Index([FromForm(Name = "file")] IFormFile file, [FromForm] string p)
+        {
+            //var s = p + "AA";
+            string s;
+            if (file != null || file.Length != 0)
+            {
+                s = "OK" + p;
+            }
+            else
+            {
+                s = "Not OK";
+            }
+            return s;
+        }
+    }
+}
